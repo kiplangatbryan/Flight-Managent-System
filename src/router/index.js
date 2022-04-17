7c7b63a3b03dcf5b876c91665dcbc3e01a186cd3
kiplangatbryan <kiplangatsgt@gmail.com>
2022-04-17 21:42:47 +0300

    added links to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/make-a-booking",
+      name: "booking",
+      component: () => import("../views/Booking.vue"),
+    },
+    {
+      path: "/about-us",
+      name: "about",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/support",
+      name: "support",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/contact-us",
+      name: "support",
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
