735d80ed24e98242dbe044605f96f92498fc9aaa
kiplangatbryan <kiplangatsgt@gmail.com>
2022-04-18 18:33:23 +0300

    added test data to pinia store

--- src/stores/main.js
+++ src/stores/main.js
@@ -4,6 +4,54 @@ export const useAppStore = defineStore({
   id: 'main',
   state: () => ({
     searchShow: false,
+    bookingState: false,
+    seats: [
+      {
+        seatId: "A01",
+        occupied: false,
+      },
+      {
+        seatId: "A02",
+        occupied: false,
+      },
+      {
+        seatId: "A03",
+        occupied: false,
+      },
+      {
+        seatId: "A04",
+        occupied: false,
+      },
+      {
+        seatId: "A05",
+        occupied: false,
+      },
+      {
+        seatId: "A06",
+        occupied: false,
+      },
+      {
+        seatId: "A07",
+        occupied: false,
+      },
+      {
+        seatId: "A08",
+        occupied: false,
+      },
+      {
+        seatId: "A09",
+        occupied: false,
+      }, {
+        seatId: "A10",
+        occupied: false,
+      }, {
+        seatId: "A11",
+        occupied: false,
+      }, {
+        seatId: "A12",
+        occupied: false,
+      },
+    ],
   }),
   getters: {
     doubleCount: (state) => state.counter * 2
