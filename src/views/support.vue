d000c40c5161a516eed86a6e1a3f90cef0be594c
kiplangatbryan <kiplangatsgt@gmail.com>
2022-05-12 08:39:45 +0300

    created

--- src/views/support.vue
+++ src/views/support.vue
@@ -1,9 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <div class="main-nav">
+      <KQNavigation />
+    </div>
+  </div>
 </template>
 
 <script>
+import KQNavigation from "@/components/KQNavigation.vue";
+
 export default {
+  components: {
+    KQNavigation,
+  },
   setup() {
     return {};
   },
@@ -11,4 +20,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.main_nav {
+  background: #000;
+  padding: 0 10em;
+}
 </style>
